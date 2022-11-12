52cc44f6048c73203491fc92f966717929fc94ae
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-12 20:59:02 +0100

    Add joi validation to register endpoint

--- src/server/routers/usersRouters/usersRouters.ts
+++ src/server/routers/usersRouters/usersRouters.ts
@@ -1,13 +1,19 @@
 import express from "express";
+import { validate } from "express-validation";
 import {
   loginUser,
   registerUser,
 } from "../../controllers/usersControllers/usersControllers.js";
+import userRegisterSchema from "../../schema/userRegisterDataSchema.js";
 
 // eslint-disable-next-line new-cap
 const usersRouter = express.Router();
 
-usersRouter.post("/register", registerUser);
+usersRouter.post(
+  "/register",
+  validate(userRegisterSchema, {}, { abortEarly: false }),
+  registerUser
+);
 usersRouter.post("/login", loginUser);
 
 export default usersRouter;
