ad469d9e5e4860b6b2028c465e0d2c5318b411db
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-15 20:44:06 +0100

    Add GET /users endpoint test

--- src/server/routers/usersRouters/usersRouters.test.ts
+++ src/server/routers/usersRouters/usersRouters.test.ts
@@ -2,10 +2,13 @@ import "../../../loadEnvironments.js";
 import request from "supertest";
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import app from "../../app.js";
 import connectDatabase from "../../../database/index.js";
 import User from "../../../database/models/User.js";
+import type { UserTokenPayload } from "../../../types/types.js";
+import { secretWord } from "../../../loadEnvironments.js";
 
 let server: MongoMemoryServer;
 
@@ -122,6 +125,45 @@ describe("Given a POST /users/login endpoint", () => {
         .expect(expectedStatus);
 
       expect(response.body).toHaveProperty("error", "Wrong credentials");
+    });
+  });
+});
+
+describe("Given a GET /users endpoint", () => {
+  describe("When it receives a request from a logged in user that is in the database with id 'abc123'", () => {
+    test("Then it should call the response method status with a 200, and a list of users", async () => {
+      const expectedStatus = 200;
+
+      const hashedPassword = await bcrypt.hash(registerData.password, 10);
+
+      const newUser = await User.create({
+        username: registerData.username,
+        password: hashedPassword,
+        email: registerData.email,
+      });
+
+      const user = await User.findOne({ username: newUser.username });
+
+      const tokenPayload: UserTokenPayload = {
+        id: user._id.toString(),
+        username: user.username,
+      };
+
+      const token = jwt.sign(tokenPayload, secretWord, { expiresIn: "2d" });
+
+      await User.create({
+        username: "pepito123",
+        password: "pepito123",
+        email: "pepito123@gmail.com",
+      });
+
+      const response = await request(app)
+        .get("/users")
+        .set("Authorization", `Bearer ${token}`)
+        .set("Content-Type", "application/json")
+        .expect(expectedStatus);
+
+      expect(response.body).toHaveProperty("users");
     });
   });
 });
